07a4348fe54b609f28653678d2445b09228dc31b
Juan Garcia <sebacho2906@gmail.com>
2023-07-13 10:56:37 -0500

    Primer commit de este proyecto

---
+++ ProyectoEF/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoEF.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public Guid CategoriaId { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public virtual ICollection<Tarea> Tareas { get; set; }
+    }
+}
---
+++ ProyectoEF/Models/TareasContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoEF.Models
+{
+    public class TareasContext: DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Tarea> Tareas { get; set; }
+
+        public TareasContext(DbContextOptions<TareasContext> options) : base(options) 
+        {
+
+        }
+
+
+    }
+}
